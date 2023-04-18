b9a73fb734bf976b94b07e542a08e4878f5de822
gmkng <ashubestmishra@gmail.com>
2023-04-18 10:30:10 +0530

    letest

---
+++ MyBars.py
@@ -0,0 +1,255 @@
+import colors
+from libqtile import qtile
+from libqtile import widget
+from libqtile.bar import Bar
+from colors import gruvbox, gruvbox2
+from unicodes import right_arrow, left_arrow, left_half_circle, lower_left_triangle
+# from spotify import Spotify
+
+terminal = "terminator"
+
+# Brightness Up 
+def brightup():
+  qtile.cmd_spawn('brightnessctl set +5%')
+# Brightness Down
+def brightdown():
+  qtile.cmd_spawn('brightnessctl set 5%-')
+
+def menu():
+  qtile.cmd_spawn('/home/abhi/.config/rofi/launchers/type-1/launcher.sh')
+
+# COLORS----------------------------*
+
+background = colors.gruvbox['bg']
+slide1 = colors.changable['slide1']
+slide2 = colors.changable['slide2']
+rand_ = colors.changable['rand3']
+menuback = colors.changable['menuback']
+timeback = colors.changable['timeback']
+netback = colors.changable['netback']
+
+mera_bar1 = Bar([
+                    widget.TextBox(
+                        text='',
+                        background = rand_,
+                        # foreground = ,
+                        fontsize = 25,
+                        mouse_callbacks = {'Button1': menu,},
+                        padding = 4,
+                        ),
+               
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = rand_,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = menuback,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+
+                    widget.GroupBox(
+                            active=colors.draculla['cm'],
+                            inactive=colors.draculla['cl'],
+                            highlight_method='line',
+                            block_highlight_text_color=colors.gruvbox['magenta'],
+                            borderwidth=0,
+                            highlight_color=menuback,
+                            background=menuback,
+                            fontsize = 25,
+                            margin_y = 1,
+                            margin_x = 1,
+                            padding_y = 0,
+                            padding_x = 3,
+                    ),
+
+                    widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = menuback,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+                    
+                    
+                    widget.CurrentLayout(
+                        #     background=gruvbox['fg0'],
+                            foreground=gruvbox['fg9']
+                    ),
+
+                    widget.WindowCount(
+                            text_format=' {num}',
+                        #     background=gruvbox['fg0'],
+                            foreground=gruvbox['fg9'],
+                            show_zero=True,
+                    ),
+                    
+                   
+                    widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+
+                    widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+
+                    widget.Spacer(),
+
+                    
+                    widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+
+                    widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide2,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+
+                    widget.Net(
+                            format = ' {down}  {up}',
+                        #     background=netback,
+                            foreground=netback
+                    ), 
+                    
+                #      widget.TextBox(
+                #             text = '',
+                #             font = "JetBrainsMono Nerd Font Mono",
+                #             background = slide1,
+                #             foreground = background,
+                #             padding = 0,
+                #             fontsize = 19
+                #     ),
+                #    widget.TextBox(
+                #             text = '',
+                #             font = "JetBrainsMono Nerd Font Mono",
+                #             background = slide1,
+                #             foreground = background,
+                #             padding = 0,
+                #             fontsize = 22
+                #     ),
+
+                #     widget.Volume(
+                #         #     background=gruvbox['fg0'],
+                #             foreground=gruvbox['dark-magenta'],
+                #             fmt = 'ﮱ  HI-RES',
+                #             mouse_callbacks = {'Button3': lambda: qtile.cmd_spawn("pavucontrol")}
+                #     ),
+
+                     widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+
+                    widget.Clock(
+                            font = "JetBrainsMono Nerd Font",
+                            foreground = timeback,
+                        #     background = ,
+                            format=' %d%b %a-%H:%M',
+                            mouse_callbacks = {'Button3': lambda: qtile.cmd_spawn("korganizer")}                            
+                    ),
+
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = background,
+                            padding = 0,
+                            fontsize = 19
+                    ),
+                   widget.TextBox(
+                            text = '',
+                            font = "JetBrainsMono Nerd Font Mono",
+                            background = slide1,
+                            foreground = rand_,
+                            padding = 0,
+                            fontsize = 22
+                    ),
+
+                    widget.Systray(
+                            background=rand_,
+                            foreground = gruvbox['fg0'],
+                            icon_size = 22,
+                    )
+                                       
+            ],
+               background=background, size=26, margin=[10, 10, 10, 10],
+        )
---
+++ colors.py
@@ -0,0 +1,94 @@
+nord_fox = {
+    'bg':               "#2e3440",
+    'fg':               "#b9bfca",
+    'fg_gutter':        "#4b5668",
+    'black':            "#3b4252",
+    'red':              "#bf616a",
+    'green':            "#a3be8c",
+    'yellow':           "#ebcb8b",
+    'blue':             "#81a1c1",
+    'magenta':          "#b48ead",
+    'cyan':             "#88c0d0",
+    'white':            "#FCF6F6",
+    'orange':           "#c9826b",
+    'pink':             "#bf88bc",
+    'red':              "#990012"
+}
+
+gruvbox = {
+    'bg':               '#000000',
+    'tbg':              '#28282800',
+    'fg':               '#d4be98',
+    'tfg':              '#d4be9880',
+    'dark-red':         '#FC260E',
+    'red':              '#ea6962',
+    'dark-green':       '#a9b665',
+    'green':            '#a9b665',
+    'dark-yellow':      '#e78a4e',
+    'yellow':           '#d8a657',
+    'dark-blue':        '#7daea3',
+    'blue':             '#03DAC6',
+    'dark-magenta':     '#d3869b',
+    'magenta':          '#B64FC8',
+    'dark-cyan':        '#89b482',
+    'cyan':             '#89b482',
+    'dark-gray':        '#665c54',
+    'gray':             '#928374',
+    'white':            '#FDF900',
+    'my_yellow':        '#328496',
+
+    'fg4':              '#766f64',
+    'fg3':              '#665c54',
+    'fg2':              '#504945',
+    'fg1':              '#3c3836',
+    'tfg1':             '#3c383680',
+    'bg0':              '#32302f',
+    'tbg0':             '#32302f80',
+    'fg0':              '#1d2021',
+    'tfg0':             '#1d202180',
+    'fg9':              '#ebdbb2'
+
+}
+
+gruvbox2 = {
+    'bg':               '#282828',
+    'fg':               '#ebdbb2',
+    'dark-red':         '#D38045`',
+    'red':              '#fb4934',
+    'dark-green':       '#98971a',
+    'green':            '#b8bb26',
+    'dark-yellow':      '#d79921',
+    'yellow':           '#fabd2f',
+    'dark-blue':        '#458588',
+    'blue':             '#83a598',
+    'dark-magenta':     '#b16286',
+    'magenta':          '#d3869b',
+    'dark-cyan':        '#689d6a',
+    'cyan':             '#8ec07c',
+    'dark-gray':        '#3d3d3d',
+    'gray':             '#666666',
+}
+
+draculla = {
+    'bg':               '#282a36',
+    'cl':               '#44475a',
+    'fg':               '#f8f8f2',
+    'cm':               '#6272a4',
+    'Cyan':             '#8be9fd',
+    'Green':            '#50fa7b',
+    'Orange':           '#ffb86c',
+    'Pink':             '#ff79c6',
+    'Purple':           '#bd93f9',
+    'Red':              '#ff5555',
+    'Yellow':           '#f1fa8c'
+}
+
+changable = {
+    'slide1':           '#FAE6C6',
+    'slide2':           '#EE4757',
+    'rand3':            '#1C616D',
+    'rand4':            '#341514',
+    'menuback':         '#EDC400',
+    'timeback':         '#cbeef3',
+    'netback':          '#FFD97F'
+}
